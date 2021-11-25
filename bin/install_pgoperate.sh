578ce84efb73fa0e63fe938234528945f42b83aa
mmuehlbeyer <mmuehlbeyer@users.noreply.github.com>
2021-11-25 15:11:26 +0100

    added pgoperate alias

--- bin/install_pgoperate.sh
+++ bin/install_pgoperate.sh
@@ -80,11 +80,14 @@ sed -i "/# pgOperate/d" $PGBASENV_BASE/etc/pgbasenv_standard.conf
 sed -i "/alias pgoperate=.*/d" $PGBASENV_BASE/etc/pgbasenv_standard.conf
 sed -i "/alias cdbase=.*/d" $PGBASENV_BASE/etc/pgbasenv_standard.conf
 sed -i "alias cdpo=.*/d" $PGBASENV_BASE/etc/pgbasenv_standard.conf
+sed -i "alias cdpo.etc=.*/d" $PGBASENV_BASE/etc/pgbasenv_standard.conf
+
 echo "
 # pgOperate
 alias pgoperate=\"\$PGOPERATE_BASE/bin/pgoperate\"
 alias cdbase='eval \"cd \$(test -f \$PGOPERATE_BASE/etc/parameters_\${PGBASENV_ALIAS}.conf && grep \"^PGSQL_BASE\" \$PGOPERATE_BASE/etc/parameters_\${PGBASENV_ALIAS}.conf | cut -d\"=\" -f2 || echo \".\")\"'
-alias cdpo='cd $PGOPERATE'
+alias cdpo='cd \$PGOPERATE_BASE'
+alias cdpo.etc='cd \$PGOPERATE_BASE/etc'
 " >> $PGBASENV_BASE/etc/pgbasenv_standard.conf
 alias 
 
